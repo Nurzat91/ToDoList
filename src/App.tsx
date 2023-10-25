673bfd88158d6004b058bec38a9d3914715764ae
Nurzat Taalaibekova <tnurzat91@gmail.com>
2023-10-25 20:53:30 +0300

    checkbox implementation.

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,14 @@ import Task from './Task/Task';
 import React, {useState} from 'react';
 import './App.css';
 
+
 function App() {
 
   const [currentTask, setCurrentTask] = useState('');
   const [tasks, setTasks] = useState([
-    {id: '1', text: 'Task 1'},
-    {id: '2', text: 'Task 2'},
-    {id: '3', text: 'Task 3'},
+    {id: '1', text: 'Создать приложение - ToDo List. Внешний вид: 1', completed: true},
+    {id: '2', text: 'Создать приложение - ToDo List. Внешний вид: 2', completed: false},
+    {id: '3', text: 'Создать приложение - ToDo List. Внешний вид: 3', completed: false},
   ]);
 
   const maxId = Math.max(...tasks.map((task) => parseInt(task.id, 10)));
@@ -37,6 +38,15 @@ function App() {
     const updatedTasks = tasks.filter((task) => task.id !== taskId);
     setTasks(updatedTasks);
   };
+  const handleToggleTask = (taskId: string) => {
+    const updatedTasks = tasks.map((task) => {
+      if (task.id === taskId) {
+        return { ...task, completed: !task.completed };
+      }
+      return task;
+    });
+    setTasks(updatedTasks);
+  };
 
   return (
     <>
@@ -46,7 +56,12 @@ function App() {
         onBtnClick={handleSubmit}
       />
       {tasks.map((task) => (
-        <Task key={task.id} text={task.text} onDelete={() => handleDeleteTask(task.id)}/>
+        <Task
+          key={task.id}
+          text={task.text}
+          completed={task.completed}
+          onToggle={() => handleToggleTask(task.id)}
+          onDelete={() => handleDeleteTask(task.id)}/>
       ))}
     </>
   );
